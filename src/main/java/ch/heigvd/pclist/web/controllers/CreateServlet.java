3cf96807eb49b1584ac0b71983ca382209e893d3
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2017-10-03 19:12:28 +0200

    Delete doPost

--- src/main/java/ch/heigvd/pclist/web/controllers/CreateServlet.java
+++ src/main/java/ch/heigvd/pclist/web/controllers/CreateServlet.java
@@ -11,9 +11,6 @@ import java.io.IOException;
  * @author Jérémie Zanone (jeremie.zanone@heig-vd.ch)
  */
 public class CreateServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/pages/create.jsp").forward(request, response);
--- src/main/java/ch/heigvd/pclist/web/controllers/HomeServlet.java
+++ src/main/java/ch/heigvd/pclist/web/controllers/HomeServlet.java
@@ -11,9 +11,6 @@ import java.io.IOException;
  * @author Jérémie Zanone (jeremie.zanone@heig-vd.ch)
  */
 public class HomeServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
